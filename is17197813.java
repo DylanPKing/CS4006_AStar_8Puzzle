c039cff33595667a8da3cd2219674e548837b06b
DylanPKing <37106032+DylanKing2018@users.noreply.github.com>
2019-02-27 22:41:20 +0000

    Some Setup

--- is17197813.java
+++ is17197813.java
@@ -1,7 +1,20 @@
+import java.util.ArrayList;
+
 public class is17197813
 {
+	private int[] end = {1, 2, 3, 4, 5, 6, 7, 8, 0};
+	private int[] start = new int[end.length];
+	private int[] current = new int[end.length];
+
 	public static void main(String[] args)
 	{
 		
+	}
+
+	public static void aStar()
+	{
+		ArrayList<Integer> open = new ArrayList<Integer>();
+		ArrayList<Integer> closed = new ArrayList<Integer>();
+
 	}
 }
